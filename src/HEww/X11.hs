b07067fb8aeedda64857636c101bb31a1a9c1d00
Aaron Staugaard <astaugaard@icloud.com>
2022-12-19 16:51:50 -0500

    fixed crash

--- src/HEww/X11.hs
+++ src/HEww/X11.hs
@@ -1,42 +1,39 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module HEww.X11 where
-
+    
 import Graphics.X11.Xlib.Event
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras
+
 import Pipes
+
 import Control.Concurrent.STM
-import Control.Concurrent.Async
+
 import HEww.Core
+
 import Data.Text (Text)
 import Data.String (fromString)
+
 import Control.Monad.Reader
-import Control.Concurrent (forkIO, runInBoundThread, forkOS)
+import Control.Concurrent (forkIO)
+import Control.Monad.State
+
 import System.IO
-import Data.Bits ((.|.))
 
 data XEnv = XEnv {display :: Display, root :: Window}
 
-type X11DataSource a = ReaderT XEnv (Pipe Event (UpdateM a ()) IO) ()
+data X11DataSource a = forall s. X11DataSource (Event -> StateT s (ReaderT XEnv IO) (Maybe (UpdateM a ()))) s
 
 windowTitle :: (Maybe Text -> UpdateM a ()) -> X11DataSource a
-windowTitle f = do dis <- asks display
-                   act <- liftIO $ internAtom dis "_NET_ACTIVE_WINDOW" False
-                   n1 <- liftIO $ internAtom dis "_NET_WM_NAME" False
-                   n2 <- liftIO $ internAtom dis "WM_NAME" False
-                   windowTitleLoop (Nothing,Nothing) act n1 n2 f
-                   liftIO $ putStrLn "end?"
-
-iterateM_ :: Monad m => (a -> m a) -> a -> m b
-iterateM_ mf s = do r <- mf s
-                    iterateM_ mf r
-
-windowTitleLoop :: (Maybe XID,Maybe String) -> Atom -> Atom -> Atom -> (Maybe Text -> UpdateM a ()) -> X11DataSource a
-windowTitleLoop lv act n1 n2 f =
-  iterateM_ (\s@(lx,_) ->
-    do ev <- lift $ await
+windowTitle f =
+  X11DataSource (\ev ->
+    do dis <- asks display
+       act <- liftIO $ internAtom dis "_NET_ACTIVE_WINDOW" False
+       n1 <- liftIO $ internAtom dis "_NET_WM_NAME" False
+       n2 <- liftIO $ internAtom dis "WM_NAME" False
        rt <- asks root
-       dis <- asks display
-       (r,sendRet) <- liftIO $ runInBoundThread $
+       s@(lx,_) <- get
+       (r,sendRet) <- liftIO $
         do r <-
             case ev of
                 PropertyEvent _ _ _ _ win atom _ _ ->
@@ -45,64 +42,61 @@ windowTitleLoop lv act n1 n2 f =
                         case activeWin of
                             Just 0 -> case lx of
                                         Just a -> do selectInput dis a noEventMask
-                                                     flush dis
                                                      return ((Nothing,Nothing),(Just Nothing))
-
                                         Nothing -> return ((Nothing,Nothing),Nothing)
-
                             Just n -> do selectInput dis n propertyChangeMask
-                                         flush dis
                                          case lx of
                                             Just a -> if a == n then
                                                         return ((Just a,Nothing),Nothing)
                                                       else do selectInput dis a noEventMask
-                                                              flush dis
-                                                              changedTitle f n dis
-                                            Nothing -> changedTitle f n dis
+                                                              changedTitle n dis
+                                            Nothing -> changedTitle n dis
                             Nothing -> do hPutStrLn stderr "could not get active window"
                                           return (s,Nothing)
                       else if atom == n1 || atom == n2 then
                         case lx of
                           Nothing -> return (s,Nothing)
                           Just lw -> if lw == win then
-                                        changedTitle f lw dis
+                                        changedTitle lw dis
                                      else return (s,Nothing)
                       else
                         return (s,Nothing)
                 _ -> return (s,Nothing)
            liftIO $ flush dis
            return r
+       put r
        case sendRet of
-           Nothing -> return ()
-           Just a -> lift $ yield $ f $ fmap fromString a
-       return r) lv
+           Nothing -> return Nothing
+           Just a -> return $ Just $ f $ fmap fromString a)
+    (Nothing,Nothing)
 
-changedTitle :: (Maybe Text -> UpdateM a ()) -> XID -> Display -> IO ((Maybe XID, Maybe String),Maybe (Maybe String))
-changedTitle f xid dis = do n <- fetchName dis xid
-                            flush dis
-                            case n of
+changedTitle :: XID -> Display -> IO ((Maybe XID, Maybe String),Maybe (Maybe String))
+changedTitle xid dis = do n <- fetchName dis xid
+                          case n of
                               Nothing -> return ((Just xid,Nothing),Nothing)
                               Just name -> return ((Just xid,Just name), Just $ Just $ name)
 
-eventLoop :: Display -> TChan (Event) -> IO ()
-eventLoop dis chan = forever $ allocaXEvent $ \ev -> do
-       nextEvent dis ev
-       ev' <- getEvent ev
-       atomically $ writeTChan chan ev'
+eventLoop :: Display -> Window -> [X11DataSource a] -> TQueue (UpdateM a ()) -> IO ()
+eventLoop dis rt dsources q =
+       allocaXEvent $ \ev -> do
+            nextEvent dis ev
+            ev' <- getEvent ev
+            dn <- forM dsources $ \(X11DataSource d s) ->
+             do (up,s') <- runReaderT (runStateT (d ev') s) (XEnv dis rt)
+                case up of
+                    Just up'-> pushInputToTQueue q up'
+                    Nothing -> return ()
+                return $ X11DataSource d s'
+            eventLoop dis rt dn q
+       -- atomically $ writeTChan chan ev'
 
 x11DataSources :: [X11DataSource a] -> ([DataSource a] -> IO a) -> [DataSource a] ->  IO a
 x11DataSources x11data = addDataSource $
     do tq <- newTQueueIO
-       itc <- newBroadcastTChanIO
        dis <- openDisplay ":0"
        let rt = defaultRootWindow dis
        selectInput dis rt (propertyChangeMask)
        flush dis
-       void $ forkOS $ eventLoop dis itc
-       forConcurrently_ x11data $ \p ->
-         do ttc <- atomically $ dupTChan itc
-            forkIO $ runEffect $ tChanToProducer ttc >->
-                                 runReaderT p (XEnv dis rt) >->
-                                 forever (await >>= (lift . pushInputToTQueue tq))
+       void $ forkIO $ eventLoop dis rt x11data tq
        return $ DataSource (tQueueToProducer tq) $ pure ()
 
